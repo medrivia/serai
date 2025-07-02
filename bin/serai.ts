382c12855393ea1c41e45e40f5205ffe940d5aed
Umar Alfarouk <medrivia@gmail.com>
2025-07-02 21:41:42 +0700

    feat: Added manpage generation

--- bin/serai.ts
+++ bin/serai.ts
@@ -1,6 +1,21 @@
 #!/usr/bin/env bun
-if (typeof Bun !== "undefined") {
+if (typeof Bun === "undefined" || !process.versions.bun) {
     throw new Error("This app needs to be executed with Bun.")
 }
+// id:assert-bun-runtime
+// #tag: detection
+// @see https://bun.sh/guides/util/detect-bun
 
+(() => {
+    const missing = ['fzf', 'less'].filter(exe => !Bun.which(exe))
+    if (missing.length > 0) {
+        throw new Error("Missing these executables: " + missing)
+    }}
+)()
+// id:assert-executables
+
+// id:closing
 console.log("Thanks for using Serai!")
+// #tag: message
+
+Bun.spawn(['fzf', '--no-hscroll'])
