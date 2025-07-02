c20c388cb5e6ba91dc8cfa40923668f66036a616
Umar Alfarouk <medrivia@gmail.com>
2025-07-02 22:31:06 +0700

    fix: Last attempt to fix manpage

--- man-generate.ts
+++ man-generate.ts
@@ -6,7 +6,7 @@ import {unified} from 'unified'
 import { version } from './package.json'
 const MAN_DIR = './man'
 
-for (const path of new Glob(`${MAN_DIR}/*.md`).scanSync('.')) {
+for (const path of new Glob(`${MAN_DIR}/**/*.md`).scanSync('.')) {
     const file = await unified()
       .use(remarkParse)
       .use(remarkMan, {version, manual: "Write with more freedom"})
