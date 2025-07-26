bd9c92bc17482d00513b26baad507cfa70c655dc
Umar Alfarouk <medrivia@gmail.com>
2025-07-26 17:39:22 +0700

    feat(cli): Utilize fzf (WIP)

--- packages/cli/bin/serai.ts
+++ packages/cli/bin/serai.ts
@@ -3,4 +3,25 @@ if (typeof Bun === 'undefined' || !process.versions.bun) {
 	throw new Error('This app needs to be executed with Bun.')
 }
 
-console.log('@serai/cli: WORK IN PROGRESS')
+import { assertBins, inlineFzf, styleText as stx } from '../utils'
+
+const EXT_BINS = ['fzf', 'less']
+assertBins(EXT_BINS)
+
+const exit = () => {
+	console.log('Keep writing!')
+	process.exit(0)
+}
+
+process.on('SIGINT', () => {
+	exit()
+})
+
+const cmd = await inlineFzf()
+
+if (!cmd) {
+	console.log(stx(['dim'], 'You didn’t choose anything.'))
+	exit()
+}
+
+console.log('you choose: ' + cmd)
